531e72fbe2fae24b17d8545252c6516417008706
maria <maximalaccident@yandex.ru>
2026-03-10 12:46:13 +0300

    Added LoanSevice class declaration

--- include/LoanService.h
+++ include/LoanService.h
@@ -1,8 +1,23 @@
-//
-// Created by Maria on 02.03.2026.
-//
-
-#ifndef LIBRARY_PROJECT_LOANSERVICE_H
-#define LIBRARY_PROJECT_LOANSERVICE_H
-
-#endif //LIBRARY_PROJECT_LOANSERVICE_H
+#pragma once
+#include <string>
+#include <vector>
+#include "BookCatalog.h"
+#include "ReaderRegistry.h"
+class LoanService {
+public:
+    struct LoanRecord {
+        std::string bookId;
+        std::string readerId;
+        bool returned = false;
+    };
+private:
+    BookCatalog& catalog_;
+    ReaderRegistry& readers_;
+    std::vector<LoanRecord> loans_;
+public:
+    LoanService(BookCatalog& catalog, ReaderRegistry& readers);
+    bool loanBook(const std::string& bookId, const std::string& readerId);
+    bool returnBook(const std::string& bookId);
+    bool isBookLoaned(const std::string& bookId) const;
+    std::vector<LoanRecord> listLoans() const;
+};
