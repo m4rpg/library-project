1c6fe88bab0725d17f268f39032a5fa38239839d
JustGeorge <georgi.g2006@gmail.com>
2026-03-11 05:10:18 +0300

    Add Book.cpp implementation

--- src/books/Book.cpp
+++ src/books/Book.cpp
@@ -1,3 +1,30 @@
 //
 // Created by Maria on 02.03.2026.
 //
+#include "Book.h"
+#include <string>
+#include <format>
+
+
+Book(std::string id, std::string title, std::string author, int year) : id(std::move(id)), title(std::move(title)),
+    author(std::move(author)), year(year) {}
+
+const std::string& Book::getId() const {
+    return id;
+}
+
+const std::string& Book::getTitle() const {
+    return title;
+}
+
+const std::string& Book::getAuthor() const {
+    return author;
+}
+
+int Book::getYear() const {
+    return year;
+}
+
+std::string Book::to_String() const {
+    return std::format("{}: {} by {} ({})", id, title, author, year);
+}
