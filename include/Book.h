fed1098fa7539d0a878f154ab52c070ef25bf6a8
JustGeorge <georgi.g2006@gmail.com>
2026-03-11 04:46:16 +0300

    Update Book.h
    Add getters and to_string to Book class

--- include/Book.h
+++ include/Book.h
@@ -5,4 +5,23 @@
 #ifndef LIBRARY_PROJECT_BOOK_H
 #define LIBRARY_PROJECT_BOOK_H
 
+#include <string>
+
+class Book {
+private:
+    std::string id;
+    std::string title;
+    std::string author;
+    int year;
+
+public:
+    Book(std::string id, std::string title, std::string author, int year);
+    
+    const std::string& getId() const;
+    const std::string& getTitle() const;
+    const std::string& getAuthor() const;
+    int getYear() const;
+    std::string to_string() const;
+};
+
 #endif //LIBRARY_PROJECT_BOOK_H
