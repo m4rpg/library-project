9724d6a1e50160ef09b785854a552159dfa092c9
JustGeorge <georgi.g2006@gmail.com>
2026-03-11 07:42:03 +0300

    Add BookCatalog.cpp implementation

--- src/books/BookCatalog.cpp
+++ src/books/BookCatalog.cpp
@@ -1,3 +1,41 @@
-//
-// Created by Maria on 02.03.2026.
-//
+#include "BookCatalog.h"
+#include <algorithm>
+
+void BookCatalog::addBook(Book book) {
+    books.push_back(std::move(book));
+}
+
+std::optional<Book> BookCatalog::searchById(const std::string& id) const {
+    for (const auto& book : books) {
+        if (book.getId == id) {
+            return book;
+        }
+    }
+    return std::nullopt;
+}
+
+bool BookCatalog::removeById(const std::string& id) const {
+    auto it = std::remove_if(books.begin(), books.end(), [&id](const Book& b) { return b.getId() == id; });
+    
+    if (it != books.end()) {
+        books.erase(it, books.end());
+        return true;
+    }
+    return false;
+}
+
+const std::vector<Book>& BookCatalog::listAll() const {
+    return books;
+}
+
+bool BookCatalog::is_existed(const std::string& id) const {
+    return searchById(id).has_value();
+}
+
+size_t BookCatalog::size() const {
+    return books.size();
+}
+
+void BookCatalog::clear() {
+    books.clear();
+}
