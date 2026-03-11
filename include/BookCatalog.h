dd800516db1b829c2d4863f4fb884b677ac84916
JustGeorge <georgi.g2006@gmail.com>
2026-03-11 07:08:24 +0300

    Update BookCatalog.h

--- include/BookCatalog.h
+++ include/BookCatalog.h
@@ -1,8 +1,24 @@
-//
-// Created by Maria on 02.03.2026.
-//
-
 #ifndef LIBRARY_PROJECT_BOOKCATALOG_H
 #define LIBRARY_PROJECT_BOOKCATALOG_H
+
+#include "Book.h"
+#include <vector>
+#include <string>
+#include <optional>
+
+class BookCatalog {
+private:
+    std::vector<Book> books;
+    
+public:
+    void addBook(Book book);
+    std::optional<Book> searchById(const std::string& id) const;
+    bool removeById(const std::string& id);
+    const std::vector<Book>& listAll() const;
+    bool is_existed(const std::string& id) const;
+    size_t size() const;
+    void clear();
+    
+};
 
 #endif //LIBRARY_PROJECT_BOOKCATALOG_H
