980e453052d359489c45ab7f01a618d1cd14ebdb
sashasubbotin067-cell <sashasubbotin067@gmail.com>
2026-03-12 14:35:19 +0300

    Adding People test

--- tests/test_people.cpp
+++ tests/test_people.cpp
@@ -1,3 +1,32 @@
-//
-// Created by Maria on 12.03.2026.
-//
+#include <gtest/gtest.h>
+#include "Librarian.h"
+#include "Reader.h"
+
+TEST(PersonTest, ReaderRoleIsCorrect) {
+    Reader reader("r1", "Maria", "maria@mail.com");
+
+    EXPECT_EQ(reader.role(), "Reader");
+}
+
+TEST(PersonTest, LibrarianRoleIsCorrect) {
+    Librarian librarian("l1", "Alexander", "EMP001");
+
+    EXPECT_EQ(librarian.role(), "Librarian");
+}
+
+TEST(PersonTest, LibrarianStoresEmployeeCode) {
+    Librarian librarian("l1", "George", "EMP777");
+
+    EXPECT_EQ(librarian.employeeCode(), "EMP777");
+}
+
+TEST(PersonTest, PolymorphismWorksForPeople) {
+    Reader reader("r1", "Maria", "maria@mail.com");
+    Librarian librarian("l1", "Alexander", "EMP001");
+
+    Person* p1 = &reader;
+    Person* p2 = &librarian;
+
+    EXPECT_EQ(p1->role(), "Reader");
+    EXPECT_EQ(p2->role(), "Librarian");
+}
