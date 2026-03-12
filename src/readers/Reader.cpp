c5bfde7a9ca7ad831576254fe3902e6bfd2ae1be
maria <maximalaccident@yandex.ru>
2026-03-12 12:46:11 +0300

    Updating Reader class

--- src/readers/Reader.cpp
+++ src/readers/Reader.cpp
@@ -1,5 +1,5 @@
 #include "Reader.h"
-#include <utility>g
+#include <utility>
 Reader::Reader(std::string id, std::string name, std::string email)
     : Person(std::move(id), std::move(name)),
       email_(std::move(email)) {}
